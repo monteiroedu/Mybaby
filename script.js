235b69eb5c775da9a61539f44057b19f89b0a6b8
monteiroedu <monteiroeduardo@outook.com>
2022-09-05 19:47:48 -0300

    update

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
      });
 
      var typed = new Typed(".typing", {
-         strings:["Bernardo", "ou Ayla Beatriz"],
+         strings:["Aylla"],
          typeSpeed:100,
          backSpeed:60,
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["Bernardo", "ou Ayla Beatriz"],
+        strings:["Aylla"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
